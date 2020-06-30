bbbaeef27a37025a2867aff171812e945db46cc8
Tim Spekking <43640883+Timmy0506@users.noreply.github.com>
2020-06-30 09:27:46 +0200

    Add files via upload

---
+++ H1/test.js
@@ -0,0 +1,19 @@
+//opdracht 1:1
+console.log("Hello World!")
+
+
+//opdracht 1:2
+for(var i = 0; i < 10; i++) {
+    console.log ("i heeft de waarde " + i);
+}
+
+//opdracht 1:3
+
+var mycar = {
+    brand: "Ford",
+    type: "Mondeo",
+    wheels: "4",
+    colour: "Blue"
+}
+
+console.log(mycar.colour);
